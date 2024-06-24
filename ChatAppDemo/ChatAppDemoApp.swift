52b3f4e7eaebdbb5f9b188f211c07771847529d8
Andy Marthin <mr.andymarthin@gmail.com>
2024-06-25 00:53:02 +0700

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Andy Marthin on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
